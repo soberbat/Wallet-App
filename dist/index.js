4a12a4d29c2731c37acb05baa2053ff95b05994a
Berat Genç <beratgenc470@gmail.com>
2022-01-04 21:10:47 +0300

    second page added

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+const generateKeyButton = document.querySelector(".generate-box-button");
+
+const generateContainer = document.querySelector(".generate-box");
+
+generateKeyButton.addEventListener("click", () => {
+  generateContainer.style.opacity = "0";
+});
