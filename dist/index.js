4371953cc016889cfa5df8bc8d24bdc29d690430
Berat Genç <beratgenc470@gmail.com>
2022-01-06 19:34:42 +0300

    adjustments before the logic

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,14 @@
 const generateKeyButton = document.querySelector(".generate-box-button");
 
 const generateContainer = document.querySelector(".generate-box");
+const keyGeneratingContainer = document.querySelector(
+  ".key-generating-section"
+);
 
 generateKeyButton.addEventListener("click", () => {
   generateContainer.style.opacity = "0";
+  setTimeout(() => {
+    generateContainer.style.display = "none";
+    keyGeneratingContainer.style.display = "block";
+  }, 400);
 });
